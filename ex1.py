142c5ea3ef1efd2b5705f750b219ab2379c173a6
akmalatnamal <60501356+akmalatnamal@users.noreply.github.com>
2021-09-14 13:30:44 +0500

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,20 @@
+#   =, ==, +, ++
+gpas = {"ItoP": 3.0, "Physics": 2.5, "Calculus": 3.5}
+
+gpas["QCR"] = 3.5
+print(gpas)
+del(gpas["Physics"])
+print(gpas)
+print(gpas.keys())
+print(gpas.values())
+print(max(gpas.values()))
+
+
+
+
+
+
+
+
+#main program
+beatles = ['Flowers', 'Ya', 'Ya', 'wood', 'leaf', 'wood', 'leaf', 'wood', 'Ya']
